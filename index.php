b922baf540bebae52425df1c1b619d4f0f5e7792
rizwanabbasi <rizwan@rizwanabbasi.com>
2020-03-10 16:10:52 +0500

    Fixed bug redundant values being fetched
    Fixed bug redundant values being fetched from database.

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 require 'vendor/autoload.php';
 $app = new Slim\App;
 //Handle Dependencies
@@ -8,16 +7,15 @@ $container = $app->getContainer();
 $container['db'] = function ($c) {
    
    try{
-	   $conn = new PDO('sqlsrv:server =localhost; Database = TestData', 'sa', '123');
+	   $conn = new PDO('sqlsrv:server =localhost; Database = Student', 'sa', '123');
 	   $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	   $app->conn = $conn;
+	   //$app->conn = $conn;
        
 	   $db = $c['settings']['db'];
        $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
        PDO::ATTR_DEFAULT_FETCH_MODE                      => PDO::FETCH_ASSOC,
        );
-       //$pdo = new PDO("sqlsrv:Server=localhost;Database=TestData", "sa", "123");
-	   echo "MSSQL DB Connection Status: Success";
+       //echo "MSSQL DB Connection Status: Success";
        return $conn;
    }
    catch(\Exception $ex){
@@ -27,46 +25,47 @@ $container['db'] = function ($c) {
 
 $app->get('/users', function ($request,$response) {
    try{
-	   $con = $this->db;
-       $sql = "SELECT * FROM Employee";
-       $result = null;
-       foreach ($con->query($sql) as $row) {
-           $result[] = $row;
-       }
-       if($result){
-           return $response->withJson(array('status' => 'true','result'=>$result),200);
-	   }else{
-           return $response->withJson(array('status' => 'Users Not Found'),422);
-       }
-              
+		$conn = new PDO('sqlsrv:server =localhost; Database = Student', 'sa', '123');
+
+		$sql = "SELECT * FROM sis";
+		 
+		//Prepare our SELECT statement.
+		$statement = $conn->prepare($sql);
+		 
+		//Execute our statement.
+		$statement->execute();
+		 
+		//Fetch our rows. Array (empty if no rows). False on failure.
+		$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+		return $response->withJson(array('status' => 'true','result'=> $rows),200);
    }
    catch(\Exception $ex){
        return $response->withJson(array('error' => $ex->getMessage()),422);
    }
-   
 });
 
 /*fetch a single user based upon id*/
 $app->get('/user/{id}', function ($request,$response) {
-   try{
-       $id     = $request->getAttribute('id');
-       $con = $this->db;
-       $sql = "SELECT * FROM Employee WHERE EmployeeId = :id";
-       $pre  = $con->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-       $values = array(
-       ':id' => $id);
-       $pre->execute($values);
-       $result = $pre->fetch();
-       if($result){ 
-		   return $response->withJson(array('status' => 'true','result'=> $result),200);
-       }else{
-		   return $response->withJson(array('status' => 'User Not Found'),422);
-       }
-      
-   }
-   catch(\Exception $ex){
-       return $response->withJson(array('error' => $ex->getMessage()),422);
-   }
+
+$id     = $request->getAttribute('id');
+
+$conn = new PDO('sqlsrv:server =localhost; Database = Student', 'sa', '123');
+
+$sql = "SELECT * FROM sis WHERE StudentId = :id";
  
+//Prepare our SELECT statement.
+$statement = $conn->prepare($sql);
+ 
+//Bind our value to the paramater :id.
+$statement->bindValue(':id', $id);
+ 
+//Execute our statement.
+$statement->execute();
+ 
+//Fetch our rows. Array (empty if no rows). False on failure.
+$rows = $statement->fetch(PDO::FETCH_ASSOC);
+
+return $response->withJson(array('status' => 'true','result'=> $rows),200);
 });
 $app->run();
