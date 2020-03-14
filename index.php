4c10b980ee235aaa6a51746c8aa169478b2777ad
rizwanabbasi <rizwan@rizwanabbasi.com>
2020-03-14 18:29:30 +0500

    Optimized code

--- index.php
+++ index.php
@@ -1,64 +1,27 @@
 <?php
 require 'vendor/autoload.php';
 $app = new Slim\App;
+
 //Handle Dependencies
 $container = $app->getContainer();
 
-$container['db'] = function ($c) {
-   
-   try{
-	   $conn = new PDO('sqlsrv:server =localhost; Database = Student', 'sa', '123');
-	   $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	   //$app->conn = $conn;
-       
-	   $db = $c['settings']['db'];
-       $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-       PDO::ATTR_DEFAULT_FETCH_MODE                      => PDO::FETCH_ASSOC,
-       );
-       //echo "MSSQL DB Connection Status: Success";
-       return $conn;
-   }
-   catch(\Exception $ex){
-       return $ex->getMessage();
-   }
-};
+//fetch a single user based upon registration number and password
 
-$app->get('/users', function ($request,$response) {
-   try{
-		$conn = new PDO('sqlsrv:server =localhost; Database = Student', 'sa', '123');
+$app->get('/user/{registrationnumber}/{password}', function ($request,$response) {
 
-		$sql = "SELECT * FROM sis";
-		 
-		//Prepare our SELECT statement.
-		$statement = $conn->prepare($sql);
-		 
-		//Execute our statement.
-		$statement->execute();
-		 
-		//Fetch our rows. Array (empty if no rows). False on failure.
-		$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+$registrationnumber     = $request->getAttribute('registrationnumber');
+$password =	$request->getAttribute('password');
 
-		return $response->withJson(array('status' => 'true','result'=> $rows),200);
-   }
-   catch(\Exception $ex){
-       return $response->withJson(array('error' => $ex->getMessage()),422);
-   }
-});
+$conn = new PDO('sqlsrv:server =localhost; Database = dbname', 'dbusernamehere', 'dbpasswordhere');
 
-/*fetch a single user based upon id*/
-$app->get('/user/{id}', function ($request,$response) {
-
-$id     = $request->getAttribute('id');
-
-$conn = new PDO('sqlsrv:server =localhost; Database = Student', 'sa', '123');
-
-$sql = "SELECT * FROM sis WHERE StudentId = :id";
+$sql = "SELECT * FROM sis WHERE RegistrationNumber = :registrationnumber AND Password = :password";
  
 //Prepare our SELECT statement.
 $statement = $conn->prepare($sql);
  
 //Bind our value to the paramater :id.
-$statement->bindValue(':id', $id);
+$statement->bindValue(':registrationnumber', $registrationnumber);
+$statement->bindValue(':password',$password);
  
 //Execute our statement.
 $statement->execute();
@@ -68,4 +31,5 @@ $rows = $statement->fetch(PDO::FETCH_ASSOC);
 
 return $response->withJson(array('status' => 'true','result'=> $rows),200);
 });
+
 $app->run();
